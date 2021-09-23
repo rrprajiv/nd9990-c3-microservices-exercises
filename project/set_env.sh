47e78cf5e7008a6f2dce0c8c276cba72d6f74623
rrprajiv <88769287+rrprajiv@users.noreply.github.com>
2021-09-23 12:01:53 +0530

    Refactor backend code

--- project/set_env.sh
+++ project/set_env.sh
@@ -1,11 +1,13 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=password1
-export POSTGRES_HOST=udagram2.csixzufepzrt.us-west-2.rds.amazonaws.com
-export POSTGRES_DB=udagram
-export AWS_BUCKET=arn:aws:s3:::udaconnect
-export AWS_REGION=us-west-2
-export AWS_PROFILE=test
-export JWT_SECRET=testing
+export POSTGRES_USERNAME=udagramrajiv
+export POSTGRES_PASSWORD=udaGRAMRAJiv
+export POSTGRES_DB=udagramdb
+export POSTGRES_HOST=udagramrajivinstance.csckvju1v5x0.us-east-2.rds.amazonaws.com
+#export AWS_BUCKET=arn:aws:s3:::udagram-rajivpandey-dev
+export AWS_BUCKET=udagram-rajivpandey-dev
+export AWS_MEDIA_BUCKET=udagram-rajivpandey-dev
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=hello
 export URL=http://localhost:8100
